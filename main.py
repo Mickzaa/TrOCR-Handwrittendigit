cc1c341b2fd40bf1120615f4e1158ae8e91a92f5
Mickzaa <arceus@hotmail.co.th>
2022-02-05 09:47:14 +0700

    add environment.yml

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ pixel_values = processor(image, return_tensors="pt").pixel_values
 generated_ids = model.generate(pixel_values)
 
 generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-
+print(generated_text)
